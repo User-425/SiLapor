e5d3d105066eeed0166387d23bf8b2f82d019f6a
User_425 <user425.itsme@gmail.com>
2025-05-18 16:35:07 +0700

    fix: update image paths to use asset helper for consistency

--- resources/views/pages/dashboard/admin.blade.php
+++ resources/views/pages/dashboard/admin.blade.php
@@ -5,48 +5,47 @@
     <meta charset="utf-8" />
     <title>SiLapor Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    @vite('resources/css/app.css')
   </head>
   <body class="bg-gray-100 font-sans">
     <div class="min-h-screen flex">
       <!-- Sidebar -->
       <div class="w-64 bg-white shadow-lg flex flex-col">
         <div class="flex items-center p-4 border-b">
-          <img class="w-10 h-10" src="img/dummy.png" alt="Logo" />
+          <img class="w-10 h-10" src="{{ asset('img/dummy.png') }}" alt="Logo" />
           <span class="ml-3 text-xl font-bold text-gray-800">SiLapor</span>
         </div>
         <div class="flex-1 p-4">
           <nav class="space-y-2">
             <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-100">
-              <img class="w-6 h-6" src="img/graph-1.png" alt="Dashboard icon" />
+              <img class="w-6 h-6" src="{{ asset('img/graph-1.png') }}" alt="Dashboard icon" />
               <span class="ml-3 text-gray-700">Dashboard</span>
             </a>
             <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-100">
-              <img class="w-6 h-6" src="img/group.png" alt="Leaderboard icon" />
+              <img class="w-6 h-6" src="{{ asset('img/group.png') }}" alt="Leaderboard icon" />
               <span class="ml-3 text-gray-700">Leaderboard</span>
             </a>
             <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-100">
-              <img class="w-6 h-6" src="img/shopping-cart.svg" alt="Order icon" />
+              <img class="w-6 h-6" src="{{ asset('img/shopping-cart.svg') }}" alt="Order icon" />
               <span class="ml-3 text-gray-700">Order</span>
             </a>
             <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-100">
-              <img class="w-6 h-6" src="img/mdi-shopping-outline.svg" alt="Products icon" />
+              <img class="w-6 h-6" src="{{ asset('img/mdi-shopping-outline.svg') }}" alt="Products icon" />
               <span class="ml-3 text-gray-700">Products</span>
             </a>
             <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-100">
-              <img class="w-6 h-6" src="img/chart-line.svg" alt="Sales Report icon" />
+              <img class="w-6 h-6" src="{{ asset('img/chart-line.svg') }}" alt="Sales Report icon" />
               <span class="ml-3 text-gray-700">Sales Report</span>
             </a>
             <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-100">
-              <img class="w-6 h-6" src="img/mdi-message-processing-outline.svg" alt="Messages icon" />
+              <img class="w-6 h-6" src="{{ asset('img/mdi-message-processing-outline.svg') }}" alt="Messages icon" />
               <span class="ml-3 text-gray-700">Messages</span>
             </a>
             <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-100">
-              <img class="w-6 h-6" src="img/mdi-cog-outline.svg" alt="Settings icon" />
+              <img class="w-6 h-6" src="{{ asset('img/mdi-cog-outline.svg') }}" alt="Settings icon" />
               <span class="ml-3 text-gray-700">Settings</span>
             </a>
             <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-100 text-red-600">
-              <img class="w-6 h-6" src="img/union.svg" alt="Sign Out icon" />
+              <img class="w-6 h-6" src="{{ asset('img/union.svg') }}" alt="Sign Out icon" />
               <span class="ml-3">Sign Out</span>
             </a>
           </nav>
@@ -59,16 +58,16 @@
           <h1 class="text-xl font-semibold text-gray-800">Dashboard</h1>
           <div class="flex items-center space-x-4">
             <div class="relative">
-              <img class="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2" src="img/magnifier.svg" alt="Search icon" />
+              <img class="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2" src="{{ asset('img/magnifier.svg') }}" alt="Search icon" />
               <input type="text" placeholder="Search here..." class="pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
             </div>
             <div class="flex items-center space-x-2">
               <div class="relative">
-                <img class="w-6 h-6" src="img/clarity-notification-line.svg" alt="Notification icon" />
-                <img class="w-3 h-3 absolute top-0 right-0" src="img/vector.svg" alt="Notification dot" />
+                <img class="w-6 h-6" src="{{ asset('img/clarity-notification-line.svg') }}" alt="Notification icon" />
+                <img class="w-3 h-3 absolute top-0 right-0" src="{{ asset('img/vector.svg') }}" alt="Notification dot" />
               </div>
               <div class="flex items-center space-x-2">
-                <img class="w-10 h-10 rounded-full" src="img/rectangle-1393.png" alt="Profile" />
+                <img class="w-10 h-10 rounded-full" src="{{ asset('img/rectangle-1393.png') }}" alt="Profile" />
                 <div>
                   <p class="text-sm text-gray-600">Admin</p>
                   <p class="text-sm font-medium text-gray-800">Musfiq</p>
