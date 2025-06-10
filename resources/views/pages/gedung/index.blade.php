85e785a10368d6746b1717afb9e4d4cd745199a6
User_425 <user425.itsme@gmail.com>
2025-06-11 00:27:51 +0700

    fix: Correct button onclick attribute for creating a new building

--- resources/views/pages/gedung/index.blade.php
+++ resources/views/pages/gedung/index.blade.php
@@ -64,9 +64,7 @@
                         </div>
                     </div>
                 </form>
-                <button type="button" onclick="window.location.href='完毕
-
-System: '{{ route('gedung.create') }}'" 
+                <button type="button" onclick="window.location.href='{{ route('gedung.create') }}'" 
         class="ml-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 flex items-center">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
