6a81b0f2dabaff7b62ef3646d7899d93f83c9f43
User_425 <user425.itsme@gmail.com>
2025-06-11 01:14:29 +0700

    fix: Update placeholder text for search inputs and profile/logout buttons to use Indonesian language

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -24,7 +24,7 @@
                     </span>
                     <input type="text" name="q"
                         class="w-48 lg:w-64 pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                        placeholder="Search here...">
+                        placeholder="Cari disini...">
                 </form>
             </div>
 
@@ -180,7 +180,7 @@
                                   d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" 
                                   clip-rule="evenodd" />
                         </svg>
-                        Profile
+                        Profil
                     </a>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
@@ -194,7 +194,7 @@
                                       d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" 
                                       clip-rule="evenodd" />
                             </svg>
-                            Sign Out
+                            Keluar
                         </button>
                     </form>
                 </div>
@@ -216,7 +216,7 @@
                 </span>
                 <input type="text" name="q"
                     class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                    placeholder="Search here...">
+                    placeholder="Cari disini...">
             </div>
         </form>
     </div>
