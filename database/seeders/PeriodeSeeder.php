c01612fb4b071f4492753b0c5b90816561b46112
User_425 <user425.itsme@gmail.com>
2025-06-11 07:45:40 +0700

    fix: Correct date ranges for periods in PeriodeSeeder

--- database/seeders/PeriodeSeeder.php
+++ database/seeders/PeriodeSeeder.php
@@ -15,30 +15,30 @@ class PeriodeSeeder extends Seeder
     {
         $periodes = [
             [
+                'nama_periode' => '2023 genap',
+                'tanggal_mulai' => '2024-01-01',
+                'tanggal_selesai' => '2024-06-30',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
                 'nama_periode' => '2024 ganjil',
+                'tanggal_mulai' => '2024-06-01',
+                'tanggal_selesai' => '2024-12-30',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'nama_periode' => '2024 genap',
                 'tanggal_mulai' => '2025-01-01',
                 'tanggal_selesai' => '2025-06-30',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
             [
-                'nama_periode' => '2024 genap',
-                'tanggal_mulai' => '2025-06-01',
-                'tanggal_selesai' => '2025-12-30',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
                 'nama_periode' => '2025 ganjil',
-                'tanggal_mulai' => '2026-01-01',
-                'tanggal_selesai' => '2026-06-30',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'nama_periode' => '2025 genap',
-                'tanggal_mulai' => '2026-07-01',
-                'tanggal_selesai' => '2026-12-31',
+                'tanggal_mulai' => '2025-07-01',
+                'tanggal_selesai' => '2025-12-31',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
