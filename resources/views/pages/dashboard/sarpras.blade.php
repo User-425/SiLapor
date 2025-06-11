91b6b4949daca3c62a240c0172c17e59f286eb91
User_425 <user425.itsme@gmail.com>
2025-06-11 23:21:12 +0700

    fix: Clean up unused code and improve overall readability in sarpras.blade.php

--- resources/views/pages/dashboard/sarpras.blade.php
+++ resources/views/pages/dashboard/sarpras.blade.php
@@ -12,8 +12,8 @@
         <div class="flex flex-wrap gap-3">
             <a href="{{ route('laporan.index', ['status' => 'menunggu_verifikasi']) }}"
                 class="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded-lg flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
                 Verifikasi Laporan ({{ $laporanStats['menunggu_verifikasi'] }})
             </a>
